c8af7d7863dfcdb453f7269290f550d35890c6dd
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-05-05 17:50:58 -0400

    take out commented clientPort variable

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ const PORT = process.env.PORT || 3000;
 const REDIS_PORT = process.env.PORT || 6379;
 
 // Create Redis client
-// const client = redis.createClient(REDIS_PORT);
+const clientPort = redis.createClient(REDIS_PORT);
 
 // Create Redis client
 const client = redis.createClient({url: process.env.REDIS_URL});
